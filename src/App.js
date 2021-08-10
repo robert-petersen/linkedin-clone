b07ec5ed9f1008f7c0232029e8fe72a2335f734f
Robert Petersen <robert.petersen808@gmail.com>
2021-08-10 18:03:25 -0500

    almost finished login

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { login, logout, selectUser } from './features/userSlice';
 import './App.css';
 import Header from './Header';
 import Sidebar from "./Sidebar";
 import Feed from './Feed';
+import Login from './Login';
+import { auth } from './firebase';
 
 function App() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged(userAuth => {
+      if (userAuth) {
+        dispatch(login({
+          email: userAuth.email,
+          uid: userAuth.uid,
+          displayName: userAuth.displayName,
+          photoUrl: userAuth.photoURL,
+        }))
+      } else {
+        dispatch(logout())
+      }
+    })
+  })
+
   return (
     <div className="app">
       <Header />
-      <div className="app__body">
-        <Sidebar />
-        <Feed />
-      </div>
+      {!user ? <Login /> : (
+        <div className="app__body">
+          <Sidebar />
+          <Feed />
+        </div>
+      )}
     </div>
   );
 }
--- src/Feed.js
+++ src/Feed.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import "./Feed.css";
 import CreateIcon from '@material-ui/icons/Create';
 import InputOption from './InputOption';
@@ -6,16 +6,47 @@ import ImageIcon from "@material-ui/icons/Image";
 import SubscriptionsIcon from "@material-ui/icons/Subscriptions";
 import EventNoteIcon from "@material-ui/icons/EventNote";
 import CalendarViewDayIcon from "@material-ui/icons/CalendarViewDay";
+import Post from './Post';
+import {db} from "./firebase";
+import firebase from "firebase";
 
 const Feed = () => {
+  const [input, setInput] = useState("");
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    db.collection("posts")
+      .orderBy("timestamp", "desc")
+      .onSnapshot(snapshot => (
+        setPosts(snapshot.docs.map(doc => (
+          {
+            id: doc.id,
+            data: doc.data(),
+          }
+        )))
+    ))
+  }, []);
+
+  const sendPost = (e) => {
+    e.preventDefault();
+    db.collection("posts").add({
+      name: "Robert Petersen",
+      description: "this is a test",
+      message: input,
+      photoUrl: "",
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+    setInput("");
+  }
+
   return (
     <div className="feed">
       <div className="feed__inputContainer">
         <div className="feed__input">
           <CreateIcon />
           <form>
-            <input type="text" />
-            <button type="submit">Send</button>
+            <input type="text" value={input} onChange={e => setInput(e.target.value)} />
+            <button onClick={sendPost} type="submit">Send</button>
           </form>
         </div>
         <div className="feed__inputOptions">
@@ -25,6 +56,11 @@ const Feed = () => {
           <InputOption Icon={CalendarViewDayIcon} title="Write Article" color="#7FC15E"/>
         </div>
       </div>
+
+      {posts.map(({ id, data: { name, description, message, photoUrl }}) => {
+        return( <Post key={id} name={name} description={description} message={message} photoUrl={photoUrl}/> );
+      })}
+
     </div>
   )
 }
